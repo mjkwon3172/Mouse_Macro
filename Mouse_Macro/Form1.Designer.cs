f599ffaa9946e8d27f609eecee909fbbeea3af66
mjkwon3172 <mjkwon9498@gmail.com>
2023-02-19 17:15:34 +0900

    ...

--- Mouse_Macro/Form1.Designer.cs
+++ Mouse_Macro/Form1.Designer.cs
@@ -28,12 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label_date = new System.Windows.Forms.Label();
+            this.Label_movetime = new System.Windows.Forms.Label();
+            this.Label_filename = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Label_enter = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Textbox_time = new System.Windows.Forms.MaskedTextBox();
+            this.Textbox_filename = new System.Windows.Forms.MaskedTextBox();
+            this.Textbox_entertime = new System.Windows.Forms.MaskedTextBox();
+            this.Btn_add = new System.Windows.Forms.Button();
+            this.Btn_del = new System.Windows.Forms.Button();
+            this.Btn_edit = new System.Windows.Forms.Button();
+            this.Btn_order = new System.Windows.Forms.Button();
+            this.Btn_start = new System.Windows.Forms.Button();
+            this.Btn_end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label_date
+            // 
+            this.Label_date.AutoSize = true;
+            this.Label_date.BackColor = System.Drawing.SystemColors.Control;
+            this.Label_date.Location = new System.Drawing.Point(27, 19);
+            this.Label_date.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label_date.Name = "Label_date";
+            this.Label_date.Size = new System.Drawing.Size(29, 12);
+            this.Label_date.TabIndex = 0;
+            this.Label_date.Text = "날짜";
+            // 
+            // Label_movetime
+            // 
+            this.Label_movetime.AutoSize = true;
+            this.Label_movetime.Location = new System.Drawing.Point(215, 19);
+            this.Label_movetime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label_movetime.Name = "Label_movetime";
+            this.Label_movetime.Size = new System.Drawing.Size(53, 12);
+            this.Label_movetime.TabIndex = 1;
+            this.Label_movetime.Text = "동작시간";
+            // 
+            // Label_filename
+            // 
+            this.Label_filename.AutoSize = true;
+            this.Label_filename.Location = new System.Drawing.Point(350, 18);
+            this.Label_filename.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label_filename.Name = "Label_filename";
+            this.Label_filename.Size = new System.Drawing.Size(41, 12);
+            this.Label_filename.TabIndex = 2;
+            this.Label_filename.Text = "파일명";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(29, 73);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 30;
+            this.dataGridView.Size = new System.Drawing.Size(323, 127);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellClick);
+            // 
+            // Label_enter
+            // 
+            this.Label_enter.AutoSize = true;
+            this.Label_enter.Location = new System.Drawing.Point(368, 77);
+            this.Label_enter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label_enter.Name = "Label_enter";
+            this.Label_enter.Size = new System.Drawing.Size(75, 12);
+            this.Label_enter.TabIndex = 4;
+            this.Label_enter.Text = "입력시간(분)";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(62, 14);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(141, 21);
+            this.dateTimePicker.TabIndex = 5;
+            // 
+            // Textbox_time
+            // 
+            this.Textbox_time.Location = new System.Drawing.Point(270, 15);
+            this.Textbox_time.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Textbox_time.Mask = "90시90분";
+            this.Textbox_time.Name = "Textbox_time";
+            this.Textbox_time.Size = new System.Drawing.Size(71, 21);
+            this.Textbox_time.TabIndex = 6;
+            this.Textbox_time.ValidatingType = typeof(System.DateTime);
+            // 
+            // Textbox_filename
+            // 
+            this.Textbox_filename.Location = new System.Drawing.Point(398, 13);
+            this.Textbox_filename.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Textbox_filename.Name = "Textbox_filename";
+            this.Textbox_filename.Size = new System.Drawing.Size(71, 21);
+            this.Textbox_filename.TabIndex = 7;
+            // 
+            // Textbox_entertime
+            // 
+            this.Textbox_entertime.Location = new System.Drawing.Point(447, 73);
+            this.Textbox_entertime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Textbox_entertime.Name = "Textbox_entertime";
+            this.Textbox_entertime.Size = new System.Drawing.Size(45, 21);
+            this.Textbox_entertime.TabIndex = 8;
+            this.Textbox_entertime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Textbox_entertime_KeyPress);
+            // 
+            // Btn_add
+            // 
+            this.Btn_add.Location = new System.Drawing.Point(29, 47);
+            this.Btn_add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_add.Name = "Btn_add";
+            this.Btn_add.Size = new System.Drawing.Size(59, 21);
+            this.Btn_add.TabIndex = 9;
+            this.Btn_add.Text = "추가";
+            this.Btn_add.UseVisualStyleBackColor = true;
+            this.Btn_add.Click += new System.EventHandler(this.Btn_add_Click);
+            // 
+            // Btn_del
+            // 
+            this.Btn_del.Location = new System.Drawing.Point(101, 48);
+            this.Btn_del.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_del.Name = "Btn_del";
+            this.Btn_del.Size = new System.Drawing.Size(59, 21);
+            this.Btn_del.TabIndex = 10;
+            this.Btn_del.Text = "삭제";
+            this.Btn_del.UseVisualStyleBackColor = true;
+            this.Btn_del.Click += new System.EventHandler(this.Btn_del_Click);
+            // 
+            // Btn_edit
+            // 
+            this.Btn_edit.Location = new System.Drawing.Point(176, 47);
+            this.Btn_edit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_edit.Name = "Btn_edit";
+            this.Btn_edit.Size = new System.Drawing.Size(59, 21);
+            this.Btn_edit.TabIndex = 11;
+            this.Btn_edit.Text = "수정";
+            this.Btn_edit.UseVisualStyleBackColor = true;
+            this.Btn_edit.Click += new System.EventHandler(this.Btn_edit_Click);
+            // 
+            // Btn_order
+            // 
+            this.Btn_order.Location = new System.Drawing.Point(248, 47);
+            this.Btn_order.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_order.Name = "Btn_order";
+            this.Btn_order.Size = new System.Drawing.Size(59, 21);
+            this.Btn_order.TabIndex = 12;
+            this.Btn_order.Text = "재정렬";
+            this.Btn_order.UseVisualStyleBackColor = true;
+            this.Btn_order.Click += new System.EventHandler(this.Btn_order_Click);
+            // 
+            // Btn_start
+            // 
+            this.Btn_start.Location = new System.Drawing.Point(369, 105);
+            this.Btn_start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_start.Name = "Btn_start";
+            this.Btn_start.Size = new System.Drawing.Size(59, 21);
+            this.Btn_start.TabIndex = 13;
+            this.Btn_start.Text = "시작";
+            this.Btn_start.UseVisualStyleBackColor = true;
+            this.Btn_start.Click += new System.EventHandler(this.Btn_start_Click);
+            // 
+            // Btn_end
+            // 
+            this.Btn_end.Location = new System.Drawing.Point(369, 137);
+            this.Btn_end.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_end.Name = "Btn_end";
+            this.Btn_end.Size = new System.Drawing.Size(59, 21);
+            this.Btn_end.TabIndex = 14;
+            this.Btn_end.Text = "종료";
+            this.Btn_end.UseVisualStyleBackColor = true;
+            this.Btn_end.Click += new System.EventHandler(this.Btn_end_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "마우스 매크로 동작 여부";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(548, 229);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_end);
+            this.Controls.Add(this.Btn_start);
+            this.Controls.Add(this.Btn_order);
+            this.Controls.Add(this.Btn_edit);
+            this.Controls.Add(this.Btn_del);
+            this.Controls.Add(this.Btn_add);
+            this.Controls.Add(this.Textbox_entertime);
+            this.Controls.Add(this.Textbox_filename);
+            this.Controls.Add(this.Textbox_time);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.Label_enter);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.Label_filename);
+            this.Controls.Add(this.Label_movetime);
+            this.Controls.Add(this.Label_date);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Mouse Macro";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Label_date;
+        private System.Windows.Forms.Label Label_movetime;
+        private System.Windows.Forms.Label Label_filename;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label Label_enter;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.MaskedTextBox Textbox_time;
+        private System.Windows.Forms.MaskedTextBox Textbox_filename;
+        private System.Windows.Forms.MaskedTextBox Textbox_entertime;
+        private System.Windows.Forms.Button Btn_add;
+        private System.Windows.Forms.Button Btn_del;
+        private System.Windows.Forms.Button Btn_edit;
+        private System.Windows.Forms.Button Btn_order;
+        private System.Windows.Forms.Button Btn_start;
+        private System.Windows.Forms.Button Btn_end;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Mouse_Macro/Form1.cs
+++ Mouse_Macro/Form1.cs
@@ -6,15 +6,284 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
 
 namespace Mouse_Macro
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo); //마우스 클릭 이벤트를 위해서 필요한 코드
+        private const int MOUSEEVENTF_MOVE = 0x8000; // 마우스 이동
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02; //마우스 왼쪽 버튼 누르기
+        private const int MOUSEEVENTF_LEFTUP = 0x04; //마우스 왼쪽 버튼 떼기
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08; //마우스 오른쪽 버튼 누르기
+        private const int MOUSEEVENTF_RIGHTUP = 0x10; //마우스 오른쪽 버튼 떼기
+
+
+        System.Timers.Timer timer_time = new System.Timers.Timer(); // timer_time(특정 시간에 작동하는 타이머)할당
+        DataTable table = new DataTable(); //DataTable table로 선언
         public Form1()
         {
             InitializeComponent();
+
+            table.Columns.Add("날짜", typeof(DateTime));
+            table.Columns.Add("동작시간", typeof(DateTime));
+            table.Columns.Add("파일명", typeof(string));
+
+            dataGridView.DataSource = table;
+
+            this.Cursor = new Cursor(Cursor.Current.Handle); //커서 사용 선언
+            this.Cursor = Cursors.Default; //커서 기본 형태(화살표)
+
+            Btn_end.Enabled = false; // 입력할 경우에 종료 버튼 비활성화.
+        }
+
+        private void Btn_add_Click(object sender, EventArgs e)
+        {
+            if(table.Rows.Count < 12)  //row값을 12개까지만 입력 가능하게 만듦 (예외처리)
+            {
+                if (Textbox_filename.Text == "") //만약 Textbox_file.Text값이 공백이라면 (예외처리)
+                {
+                    MessageBox.Show("파일명을 입력해주세요", "파일명이 입력되지 않았습니다.");
+                    return; // 리턴으로 돌리기
+                }
+                if(Textbox_time.Text =="  시  분") // 시간을 입력 하지않았을 경우, (예외처리)
+                {
+                    MessageBox.Show("시간을 입력해주세요.", "시간이 입력되지 않았습니다."); 
+                    return;
+                }
+                else if(Textbox_time.Text == " 1시  분" || Textbox_time.Text == "1 시  분" || Textbox_time.Text == " 2시  분" || Textbox_time.Text == "2 시  분" || Textbox_time.Text == " 3시  분" || Textbox_time.Text == "3 시  분"
+                          || Textbox_time.Text == " 4시  분" || Textbox_time.Text == "4 시  분" || Textbox_time.Text == " 5시  분" || Textbox_time.Text == "5 시  분" || Textbox_time.Text == " 6시  분" || Textbox_time.Text == "6 시  분" || Textbox_time.Text == " 7시  분" || Textbox_time.Text == "7 시  분"
+                          || Textbox_time.Text == " 8시  분" || Textbox_time.Text == "8 시  분" || Textbox_time.Text == " 9시  분" || Textbox_time.Text == "9 시  분" || Textbox_time.Text == "10시  분" || Textbox_time.Text == "11시  분" || Textbox_time.Text == "12시  분" || Textbox_time.Text == "13시  분"
+                          || Textbox_time.Text == "14시  분" || Textbox_time.Text == "15시  분" || Textbox_time.Text == "16시  분" || Textbox_time.Text == "17시  분" || Textbox_time.Text == "18시  분" || Textbox_time.Text == "19시  분" || Textbox_time.Text == "20시  분" || Textbox_time.Text == "21시  분"
+                          || Textbox_time.Text == "22시  분" || Textbox_time.Text == "23시  분" || Textbox_time.Text == "24시  분") // (예외처리) => 많은 게 아니라 전부 다 예외처리 가능
+                {
+                    MessageBox.Show("분을 입력해주세요.", "분이 입력되어 있찌 않습니다.");
+                    return;
+                }
+                if(dateTimePicker.Value == null) // datetimepicker의 값이 없으면
+                {
+                    MessageBox.Show("날짜를 선택하여 주세요.", "날짜가 선택되지 않았습니다.");
+                    return;
+                }
+
+                DateTime txt = Convert.ToDateTime(Textbox_time.Text);
+                Textbox_time.Text = txt.ToString("HH시mm분");
+
+                for (int i = 0; i < table.Rows.Count; i++) //for 반복문을 통해 datagridview의 table을 읽어주기
+                {
+                     if(dataGridView.Rows[i].Cells[0].Value.ToString() == dateTimePicker.Value.ToString()
+                        && dataGridView.Rows[i].Cells[1].Value.ToString() == txt.ToString())
+                    {
+                        MessageBox.Show("동일한 시간대가 존재합니다.", "동일한 시간 존재");
+                        return;
+                    }
+                } 
+                table.Rows.Add(dateTimePicker.Value, Textbox_time.Text, Textbox_filename.Text);
+                dataGridView.Columns[0].DefaultCellStyle.Format = "yyyy/MM/dd";
+                dataGridView.Columns[1].DefaultCellStyle.Format = "HH: mm";
+                dataGridView.FirstDisplayedScrollingRowIndex = dataGridView.Rows.Count - 1; //스크롤
+
+                foreach(DataGridViewRow row in dataGridView.Rows)
+                {
+                    row.HeaderCell.Value = string.Format("{0}", row.Index + 1); //format을 index+1(0부터시작하기에 +1)
+                }
+            }
+            else
+            {
+                MessageBox.Show("12개까지만 입력이 가능합니다.", "갯수를 확인해주세요");
+                return;
+            }
+
+        }
+
+        private void Btn_del_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0) // 선택하는 row의 갯수가 0보다 큰 경우
+            {
+                int iselectrow = dataGridView.SelectedRows[0].Index; //선택될 row를 선언
+                dataGridView.Rows.Remove(dataGridView.Rows[iselectrow]); //iselectrow=>선택한 row 삭제 
+
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    row.HeaderCell.Value = string.Format("{0}", row.Index + 1); //format을 index+1(0부터시작하기에 +1)
+                }
+                return;
+            }
+                if(dataGridView.Rows.Count == 0) //row가 없는 경우(예외처리)
+                {
+                    MessageBox.Show("삭제할 열이 존재하지 않습니다.","열의 존재 여부 확인");
+                    return; 
+                }
+                if(dataGridView.SelectedRows.Count == 0) //row가 선택되지 않았을 경우(예외처리)
+                {
+                    MessageBox.Show("열이 선택되지 않았습니다.", "열의 선택 여부 확인");
+                    return;
+                }
+            
+        }
+
+        private void Btn_edit_Click(object sender, EventArgs e)
+        {
+            DateTime txt = Convert.ToDateTime(Textbox_time.Text);
+            Textbox_time.Text = txt.ToString("HH시mm분");
+
+            for (int c = 0; c < table.Rows.Count; c++)
+            {
+                if(dataGridView.Rows[c].Cells[0].Value.ToString() == dateTimePicker.Value.ToString() 
+                    && dataGridView.Rows[c].Cells[1].Value.ToString() == txt.ToString())
+                {
+                    MessageBox.Show("동일한 시간대가 존재합니다.");
+                    return;
+                }
+              
+            }
+            int iselectrow2 = dataGridView.SelectedRows[0].Index;
+            DataGridViewRow editrow = dataGridView.Rows[iselectrow2];
+            editrow.Cells[0].Value = dateTimePicker.Value;
+            editrow.Cells[1].Value = Textbox_time.Text;
+            editrow.Cells[2].Value = Textbox_filename.Text;
+            return;
+        }
+
+        private void Btn_order_Click(object sender, EventArgs e)
+        {
+            for(int a = 0; a < table.Rows.Count; a++)
+            {   
+                for(int b = 0; b < table.Rows.Count; b++)
+                {
+                    if (dataGridView.Rows[a].Cells[0].Value.ToString() == dataGridView.Rows[b].Cells[0].Value.ToString()) //날짜가 같은경우에는 
+                    {
+                        table.DefaultView.Sort = "동작시간 ASC"; //동작시간의 정렬 확인
+                        foreach (DataGridViewRow row in dataGridView.Rows)
+                        {
+                            row.HeaderCell.Value = string.Format("{0}", row.Index + 1); //format을 index+1(0부터시작하기에 +1)
+                        }
+                    }
+                    if(dataGridView.Rows[a].Cells[1].Value.ToString() == dataGridView.Rows[b].Cells[1].Value.ToString()) //동작시간이 같은 경우에는
+                    {
+                        table.DefaultView.Sort = "날짜 ASC"; //날짜의 정렬을 확인
+                        foreach (DataGridViewRow row in dataGridView.Rows)
+                        {
+                            row.HeaderCell.Value = string.Format("{0}", row.Index + 1); //format을 index+1(0부터시작하기에 +1)
+                        }
+                    }
+                } 
+            }
+        }
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            if(dataGridView.Rows.Count == 0) //열이 존재하지 않을 경우(예외처리)
+            {
+                MessageBox.Show("입력값이 없습니다.", "확인");
+                return;
+            }
+            if(Textbox_entertime.Text == "") //입력시간(분)이 없을 경우(예외처리)
+            {
+                MessageBox.Show("입력시간(분) 이 없습니다.", "확인");
+                return;
+            }
+            label1.Text = "마우스 매크로 동작중";
+
+            timer_time.Interval = 1000; //1초 간격으로 시간 받기
+            timer_time.Elapsed += new ElapsedEventHandler(Timer_time_Elapsed);
+            timer_time.AutoReset = true; //timer_time이벤트를 반복적으로 발생 (false => 간격으로 한번만 발생)
+            timer_time.Enabled = true; //timer_time 실행 활성화
+
+            Btn_add.Enabled = false;
+            Btn_del.Enabled = false;
+            Btn_edit.Enabled = false;
+            Btn_order.Enabled = false;
+            Btn_start.Enabled = false;
+            dateTimePicker.Enabled = false;
+
+            Btn_end.Enabled = true;
+            Textbox_entertime.Enabled = false;
+            Textbox_filename.Enabled = false;
+            Textbox_time.Enabled = false;
+
+        }
+
+        private void Timer_time_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime current_time = DateTime.Now; // 현재시간 선언
+            for (int j = 0; j < table.Rows.Count; j++) //반복문을 통해 datagridview의 [날짜][동작시간]의 값을 읽음
+            {
+                DateTime picker_date = Convert.ToDateTime(dataGridView.Rows[j].Cells[0].Value.ToString());
+                DateTime text_time = Convert.ToDateTime(dataGridView.Rows[j].Cells[1].Value.ToString());
+
+                if(current_time.Date == picker_date.Date && current_time.Hour == text_time.Hour && current_time.Minute == text_time.Minute && current_time.Second == 0)
+                { // 현재시간과 입력된 동작 시간을 비교한후, 만약 전부 같은 경우, if문 실행한다.
+                    Cursor.Position = new Point(300, 350);
+                    mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 1, 1, 0, 0);
+                    Thread.Sleep(5000);
+
+                    int text_timeenter = Convert.ToInt32(Textbox_entertime.Text); //입력시간(분)을 convert를 사용해 int로 변환
+                    Thread.Sleep(text_timeenter * 1000); //일단 초 단위로 받는다.
+
+                    Cursor.Position = new Point(400, 350);
+                    mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 1, 1, 0, 0);
+                    Thread.Sleep(5000);
+                }
+            }
+            
+        }
+
+        private void Btn_end_Click(object sender, EventArgs e) //타이머 이벤트가 다돌고 나서 
+        {
+            DialogResult dialog = MessageBox.Show("종료하시겠습니까?", "종료여부", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(dialog == DialogResult.Yes)
+            {
+                label1.Text = "마우스 매크로 종료";
+                Btn_add.Enabled = true;
+                Btn_del.Enabled = true;
+                Btn_edit.Enabled = true;
+                Btn_order.Enabled = true;
+                Btn_start.Enabled = true;
+                dateTimePicker.Enabled = true;
+
+                Btn_end.Enabled = true;
+                Textbox_entertime.Enabled = true;
+                Textbox_filename.Enabled = true;
+                Textbox_time.Enabled = true;
+            }
+            else if(dialog == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void Textbox_entertime_KeyPress(object sender, KeyPressEventArgs e) //숫자만 입력할 수 있게 하기 위한 메소드
+        {
+            if(!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e) //셀 클릭했을 경우 -> 텍스트박스에 띄울수 있게 
+        {
+            if(this.dataGridView.Rows.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                DataGridViewRow row2 = this.dataGridView.Rows[e.RowIndex];
+                dateTimePicker.Value = Convert.ToDateTime(dataGridView.SelectedRows[0].Cells[0].Value.ToString());
+
+                DateTime cell_date = Convert.ToDateTime(dataGridView.SelectedRows[0].Cells[1].Value.ToString());
+                Textbox_time.Text = cell_date.ToString("HH시mm분");
+
+                Textbox_filename.Text = row2.Cells[2].Value.ToString();
+            }
         }
     }
 }
